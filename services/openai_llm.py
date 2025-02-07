d9200a26c9959fd8c180dc5126da211950dca02c
Huy Ho <huynino0496@gmail.com>
2025-02-08 06:49:55 +0700

    Removed .env file

--- services/openai_llm.py
+++ services/openai_llm.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-openai.api_key = os.getenv("k-proj-IZqieVr1h1HMxu5f3TOHTZKUqwBEnqJNWB6Qevi3S6IgXaz67vaMGXrGaG20sD1VGbYrHjp4urT3BlbkFJGoPnzv0e5k_TTI0FSU1gLAl-gZE0qzi3fqlsZM7aviSagVnj8Nz2aaXU5l6fM01afiwes-qXAA")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #openai.api_key = OPENAI_API_KEY
 
